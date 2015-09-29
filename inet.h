0d50e5c4a12d5469bf4671978560f4155a779aca
thiaga4543 <tiaga6180263@gmail.com>
2015-09-28 22:47:09 -0700

    Create inet.h

---
+++ inet.h
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
+#define SERV_TCP_PORT 45455
+
